607e50dc27d7a196b155d1e1cb29b3fbcefc5eae
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-26 18:42:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ const k_muppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds 1. 
-const [moo, neigh, baa, oink, cluck] = 'cow horse sheep pig chicken'.split(" ")
+const [moo, bark, naa, oink, cherp] = 'cow dog sheep pig bird'.split(" ")
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
-
+const [Bob, Bobby, Bobolishos, Bobsy] = 'cow dog sheep pig'.split(" ")
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
 
 // Arrays
